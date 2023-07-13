f28c3551e85306a5e60437c326bb8a93c10cd0fe
Lisandra Gonzalez <lisandragonzalez@MacBook-Pro-4.home>
2023-07-13 17:22:02 -0400

    made updates to include classes

--- Final_Project.py
+++ Final_Project.py
@@ -8,20 +8,21 @@ def intro_message():
 #Importing a quiz dictionary with my questions
 from questions import quiz
 
+class Rules:
 #Creating a way to automatically tell player if their score is correct
-def check_ans(question, ans, attempts, score):
-	if quiz[question]['answer'].lower() == ans.lower():
-		print(f"You are correct! \nYour score is {score +1}!")
-		return True
-	else:
-		print(f"That is the wrong answer :( \nYou have {attempts - 1} left! \nTry again...")
-		return False
+	def check_ans(question, ans, attempts, score):
+		if quiz[question]['answer'].lower() == ans.lower():
+			print(f"You are correct! \nYour score is {score +1}!")
+			return True
+		else:
+			print(f"That is the wrong answer :( \nYou have {attempts - 1} left! \nTry again...")
+			return False
 
 #Creating a way to allow players to have access to answer key
-def print_dictionary():
-	for question_id, ques_answer in quiz.items():
-		for key in ques_answer:
-			print(key + ':', ques_answer[key])
+	def print_dictionary():
+		for question_id, ques_answer in quiz.items():
+			for key in ques_answer:
+				print(key + ':', ques_answer[key])
 
 #The game setup, adding an attempts rule where user can retry an answer 3 times and be alerted how many attempts they have left as well as a skip function
 intro = intro_message()
